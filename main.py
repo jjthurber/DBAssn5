47dccb89c7a9ade70cb0e57bd48a10ae7a82f519
mbaumaister13@gmail.com <mbaumaister13@gmail.com>
2019-12-12 19:29:49 -0500

    Insert form done (does not insert for some reason)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sqlite3
 from urlparse import urlparse
 from sqlite3 import Error
 from flask_bootstrap import Bootstrap
+from datetime import datetime
 
 #Sources used:
 #https://www.sqlitetutorial.net/sqlite-python/sqlite-python-select/
@@ -32,9 +33,13 @@ def get_flower_page(f):
     connection = new_connection(_db_name_)
     sightings = enumerate(query_db(connection, "SELECT * FROM sightings WHERE name=? ORDER BY datetime(sighted) DESC LIMIT 10", (f,)))
     latin_name = query_db(connection, "SELECT genus, species FROM flowers WHERE comname=?", (f,))
-    
+
     if(request.method == 'POST'):
-        update_db(connection, "UPDATE flowers SET genus=?, species=? WHERE comname=?", (request.form.get('genus'), request.form.get('species'), f,))
+        if('update' in request.form):
+            update_db(connection, "UPDATE flowers SET genus=?, species=? WHERE comname=?", (request.form.get('genus'), request.form.get('species'), f,))
+        elif('insert' in request.form):
+            sighted = datetime.strptime(request.form.get('sighted'), '%Y-%m-%d')
+            insert_db(connection, "INSERT INTO sightings(name, person, location, sighted) VALUES(?,?,?,?)", (f, request.form.get('person'), request.form.get('location'), sighted.date(),))
         return redirect(request.url)
     return render_template('flower.html', f=f, genus=latin_name[0][0], species=latin_name[0][1], sightings=sightings)
 
@@ -49,10 +54,15 @@ def query_db(connection, query, var):
 
 def update_db(connection, update, vars):
     cursor = connection.cursor()
+    print("New Genus: " + vars[0] + "\nNew Species: " + vars[1])
     cursor.execute(update, vars)
     connection.commit()
 
-# def insert_db(connection, insert):
+def insert_db(connection, insert, vars):
+    cursor = connection.cursor()
+    print("New sighting of " + vars[0] + " by " + vars[1] + " at " + vars[2] + " on " + datetime.strftime(vars[3], '%Y-%m-%d') + ".")
+    cursor.execute(insert, vars)
+    return cursor.lastrowid
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
